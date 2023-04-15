f8bc95a58d64fc017c61176cead4c13fc6293372
Michael Marshall <104014674+MikeMarsha11@users.noreply.github.com>
2023-04-15 14:52:43 -0500

    initial

--- components/experience.js
+++ components/experience.js
@@ -14,7 +14,7 @@ export default function Experience() {
               <h3>Head Graphic Designer</h3>
               <h4>TFOP</h4>
               <h4>Houston, Texas</h4>
-              <p>Responsible for creating visually stunning designs that effectively communicate a message or idea to a target audience. Using knowledge of design principles and software I collaborate with cross-functional teams to create graphics, layouts, and other visual elements for digital and print media. My portfolio showcases your mastery of typography, color theory, and composition, and demonstrates my ability to think creatively and strategically to solve design challenges.</p>
+              <p>Responsible for creating visually stunning designs that effectively communicate a message or idea to a target audience. Using knowledge of design principles and software I collaborate with cross-functional teams to create graphics, layouts, and other visual elements for digital and print media. My portfolio showcases my mastery of typography, color theory, and composition, and demonstrates my ability to think creatively and strategically to solve design challenges.</p>
             </div>
           </div>
           <div className="item">
