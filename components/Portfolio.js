ff250739bccf4811d1d2121d15b44960a5102baf
Michael Marshall <104014674+MikeMarsha11@users.noreply.github.com>
2023-04-17 11:31:46 -0500

    when opening instagram pages new tab

--- components/Portfolio.js
+++ components/Portfolio.js
@@ -3,12 +3,12 @@ export default function Portfolio() {
         <section id="portfolio">
             <h2>Portfolio</h2>
             <div className="portfolio-container">
-                <a href="https://www.instagram.com/p/CRNGRfKlU2Z/"> <img src="/img/p1.png" alt=""/></a>
-                <a href="https://www.instagram.com/p/CpVtvnWvgBW/"> <img src="/img/p2.png" alt=""/></a>
-                <a href="https://www.instagram.com/p/CqqQ5ugrGSQ/"> <img src="/img/p3.png" alt=""/></a>
-                <a href="https://www.instagram.com/p/Cp0m3trPCBA/"> <img src="/img/p4.png" alt=""/></a>
-                <a href="https://www.instagram.com/p/Cpid838LN5A/"> <img src="/img/p5.png" alt=""/></a>
-                <a href="https://www.instagram.com/p/Cqli2jfPwHf/"> <img src="/img/p6.png" alt=""/></a>
+                <a href="https://www.instagram.com/p/CRNGRfKlU2Z/" target="_blank"> <img src="/img/p1.png" alt=""/></a>
+                <a href="https://www.instagram.com/p/CpVtvnWvgBW/" target="_blank"> <img src="/img/p2.png" alt=""/></a>
+                <a href="https://www.instagram.com/p/CqqQ5ugrGSQ/" target="_blank"> <img src="/img/p3.png" alt=""/></a>
+                <a href="https://www.instagram.com/p/Cp0m3trPCBA/" target="_blank"> <img src="/img/p4.png" alt=""/></a>
+                <a href="https://www.instagram.com/p/Cpid838LN5A/" target="_blank"> <img src="/img/p5.png" alt=""/></a>
+                <a href="https://www.instagram.com/p/Cqli2jfPwHf/" target="_blank"> <img src="/img/p6.png" alt=""/></a>
             </div>
         </section>
     );
