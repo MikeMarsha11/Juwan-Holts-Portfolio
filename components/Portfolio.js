4ed7ef68fbb0956e266b1610674410757b082d9e
Michael Marshall <104014674+MikeMarsha11@users.noreply.github.com>
2023-04-17 09:36:06 -0500

    insta links on port and footer/ iphone

--- components/Portfolio.js
+++ components/Portfolio.js
@@ -3,12 +3,12 @@ export default function Portfolio() {
         <section id="portfolio">
             <h2>Portfolio</h2>
             <div className="portfolio-container">
-                <a href=""> <img src="/img/p1.png" alt=""/></a>
-                <a href=""> <img src="/img/p2.png" alt=""/></a>
-                <a href=""> <img src="/img/p3.png" alt=""/></a>
-                <a href=""> <img src="/img/p4.png" alt=""/></a>
-                <a href=""> <img src="/img/p5.png" alt=""/></a>
-                <a href=""> <img src="/img/p6.png" alt=""/></a>
+                <a href="https://www.instagram.com/p/CRNGRfKlU2Z/"> <img src="/img/p1.png" alt=""/></a>
+                <a href="https://www.instagram.com/p/CpVtvnWvgBW/"> <img src="/img/p2.png" alt=""/></a>
+                <a href="https://www.instagram.com/p/CqqQ5ugrGSQ/"> <img src="/img/p3.png" alt=""/></a>
+                <a href="https://www.instagram.com/p/Cp0m3trPCBA/"> <img src="/img/p4.png" alt=""/></a>
+                <a href="https://www.instagram.com/p/Cpid838LN5A/"> <img src="/img/p5.png" alt=""/></a>
+                <a href="https://www.instagram.com/p/Cqli2jfPwHf/"> <img src="/img/p6.png" alt=""/></a>
             </div>
         </section>
     );
