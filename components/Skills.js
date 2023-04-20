c8ab4472b96679248147ea20af4b35312f7e505e
Michael Marshall <104014674+MikeMarsha11@users.noreply.github.com>
2023-04-20 09:05:43 -0500

    changing order of skills

--- components/Skills.js
+++ components/Skills.js
@@ -6,12 +6,12 @@ export default function Skills() {
                 <h2>My Skills</h2>
                 <div className={"skills-container"}>
                   <div className="skill-box"><img src="/img/ps.png" alt="" /><span>Photoshop</span></div>
+                  <div className="skill-box"><img src="/img/pr.png" alt="" /><span>Premiere Pro</span></div>  
                   <div className="skill-box"><img src="/img/ae.png" alt="" /><span>After Effects</span></div>
                   <div className="skill-box"><img src="/img/ai.png" alt="" /><span>Illustrator</span></div>
                   <div className="skill-box"><img src="/img/id.png" alt="" /><span>In Design</span></div>
                   <div className="skill-box"><img src="/img/lr.png" alt="" /><span>Light Room</span></div>
                   <div className="skill-box"><img src="/img/aa.png" alt="" /><span>Adobe Acrobat</span></div>
-                  <div className="skill-box"><img src="/img/pr.png" alt="" /><span>Premiere Pro</span></div>  
                   <div className="skill-box"><img src="/img/pc.png" alt="" /><span>Procreate</span></div>
 
             </div> 
